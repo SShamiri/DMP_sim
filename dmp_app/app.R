f969ba2fa1374fc4cb4e318b7b5dffb9b938207a
Shamiri <sam.shamiri@skillscommission.gov.au>
2021-12-16 05:45:50 +0000

    update ui

--- dmp_app/app.R
+++ dmp_app/app.R
@@ -1,61 +1,79 @@
+library(shiny);library(shinydashboard);library(shinyWidgets)
 
-library(shiny)
-library(htmlwidgets)
-library(shinyWidgets)
-
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-
-    # Application title
-    titlePanel("DMP model Simualtion"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        
-        sidebarPanel(
-            radioGroupButtons(
-                inputId = "param_increase",
-                label = "Simualtion",
-                #selected = "",
-                #choices = c("y","A", "κ", "β", "b","λ"),
-                c("steatdy state" = "ss","y"= "prod", "A" = "a",
-                  "κ" = "kappa", "β" = "beta",
-                  "b" = "b", "λ" = "lambda"),
-                checkIcon = list(
-                    yes = tags$i(class = "fa fa-check-square",
-                                 style = "color: steelblue"),
-                    no = tags$i(class = "fa fa-square-o",
-                                style = "color: steelblue"))
+ui <- dashboardPage(
+    dashboardHeader(title = "S&M model Simualtion", titleWidth = 550, disable = FALSE),
+    dashboardSidebar(disable = TRUE),
+    dashboardBody(
+        fluidRow(
+            column(width = 4,
+                   box(title = "Info", status = "success", solidHeader = TRUE, width = 12,
+                       collapsible = FALSE, collapsed = FALSE,
+                       p("Direction:"),
+                       switchInput(
+                           inputId = "swt",
+                           label = "",
+                           value = TRUE,
+                           onLabel = "Increase",
+                           offLabel = "Decrease",
+                           onStatus = "success", 
+                           offStatus = "danger"
+                       ),
+                       radioGroupButtons(
+                           inputId = "param_increase",
+                           label = "Simualtion",
+                           c("steatdy state" = "ss","y"= "prod", "A" = "a",
+                             "κ" = "kappa", "β" = "beta",
+                             "b" = "b", "λ" = "lambda"),
+                           checkIcon = list(
+                               yes = tags$i(class = "fa fa-check-square",
+                                            style = "color: steelblue"),
+                               no = tags$i(class = "fa fa-square-o",
+                                           style = "color: steelblue"))
+                       )
+                       
+                   )
             ),
-            #verbatimTextOutput("param"),
-            radioGroupButtons(
-                inputId = "param_decline",
-                label = "Simualtion",
-                #selected = "",
-                #choices = c("y","A", "κ", "β", "b","λ"),
-                c("steatdy state" = "ss","y"= "prod", "A" = "a",
-                  "κ" = "kappa", "β" = "beta",
-                  "b" = "b", "λ" = "lambda"),
-                checkIcon = list(
-                    yes = tags$i(class = "fa fa-check-square",
-                                 style = "color: steelblue"),
-                    no = tags$i(class = "fa fa-square-o",
-                                style = "color: steelblue"))
-            ),
-        ),
-        mainPanel(
-            fluidRow(
-                column(6, echarts4rOutput("bc_plot")),
-                column(6, echarts4rOutput("ws_vs_plot"))
+            column(width = 8,
+                   fluidRow(
+                       box(title = "DMP framework", status = "success", solidHeader = TRUE, width = 12,
+                           collapsible = TRUE, collapsed = FALSE,
+                           p("The DMP model of unemployment is widely accepted as the most realistic account of unemployment based on a careful
+                             and full statement of the underlying economic principles governing labour turnover, job-finding rates, 
+                             and wage determination. Its building blocks are;"),
+                           tags$ul(
+                               tags$li("Two type of agents:",
+                                       tags$ul(tags$li("workers"),
+                                       tags$li("Firm"))),
+                               tags$li("Workers has two possible states:",
+                                       tags$ul(tags$li("Unemployed: obtain a flow utility of unemployed benefit"),
+                                               tags$li("Employed: obtain a flow utility of wage"))),
+                               tags$li("Firms has two possible states:",
+                                       tags$ul(tags$li("Vacant"),
+                                               tags$li("Employing a worker"))),
+                               tags$li("Workers and firms try to match and form employment relationships.") 
+                           )
+                       )
+                   ),
+                   fluidRow(
+                       box(title = "Wage and Vacancy curve", status = "success", solidHeader = TRUE, width = 6,
+                           collapsible = FALSE, collapsed = FALSE,
+                           echarts4rOutput("ws_vs_plot")
+                       ),
+                       box(title = "Beveridge curve", status = "success", solidHeader = TRUE, width = 6,
+                           collapsible = FALSE, collapsed = FALSE,
+                           echarts4rOutput("bc_plot")
+                       )
+                   )
             )
         )
     )
+    
+    
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
     
-   # output$param <- renderPrint({ input$param_increase })
+    # output$param <- renderPrint({ input$param_increase })
     
     # bc plot
     output$bc_plot <- renderEcharts4r({
@@ -64,7 +82,7 @@ server <- function(input, output) {
         
         dat %>% filter(between(u,u_range[1], u_range[2])) %>%
             e_chart(u_bc) %>% 
-            e_line(theta_bc, lineStyle = list(type = "solid", color = "#3D3DDD"),symbol= 'none') %>%
+            e_line(theta_bc, lineStyle = list(type = "solid", color = "#3D3DDD"),symbol= 'none', name = 'BC') %>%
             e_x_axis(min = 0.06) %>% 
             e_hide_grid_lines() %>% 
             e_mark_p(
@@ -87,12 +105,12 @@ server <- function(input, output) {
                 ),
                 lineStyle = list(type = "solid", color = "gray")
             ) %>% 
-        e_tooltip()
+            e_tooltip()
     })
     
     observeEvent(input$param_increase, {
         u_range <- c(0.08,0.16)
-
+        
         if(grepl("ss|kappa|b|beta|y",input$param_increase)){
             echarts4rProxy("bc_plot", data = dat %>% filter(between(u,u_range[1], u_range[2])), x = u_bc) %>%
                 e_remove_serie("theta_bc_a_plus") %>%
@@ -101,19 +119,19 @@ server <- function(input, output) {
                 e_remove_serie("theta_bc_lambda_minus") %>%
                 e_execute()
         }
-
+        
         if(input$param_increase == 'a'){
             echarts4rProxy("bc_plot", data = dat %>% filter(between(u,u_range[1], u_range[2])), x = u_bc) %>%
-                e_line(theta_bc_a_plus, lineStyle = list(type = "dashed", color = "#3D3DDD"),symbol= 'none') %>%
+                e_line(theta_bc_a_plus, lineStyle = list(type = "dashed", color = "#3D3DDD"),symbol= 'none', name = 'BC') %>%
                 e_remove_serie("theta_bc_a_minus") %>%
                 e_remove_serie("theta_bc_lambda_plus") %>%
                 e_remove_serie("theta_bc_lambda_minus") %>%
                 e_execute()
         }
-
+        
         if(input$param_increase == 'lambda'){
             echarts4rProxy("bc_plot", data = dat %>% filter(between(u,u_range[1], u_range[2])), x = u_bc) %>%
-                e_line(theta_bc_lambda_plus, lineStyle = list(type = "dashed", color = "#3D3DDD"),symbol= 'none') %>%
+                e_line(theta_bc_lambda_plus, lineStyle = list(type = "dashed", color = "#3D3DDD"),symbol= 'none', name = 'BC') %>%
                 e_remove_serie("theta_bc_a_plus") %>%
                 e_remove_serie("theta_bc_a_minus") %>%
                 e_remove_serie("theta_bc_lambda_minus") %>%
@@ -128,8 +146,8 @@ server <- function(input, output) {
         
         dat %>% filter(between(w,w_range[1], w_range[2])) %>%
             e_chart(w) %>% 
-            e_line(theta_ws, lineStyle = list(type = "solid", color = "#8d89f5"),symbol= 'none') %>%
-            e_line(theta_vs, lineStyle = list(type = "solid", color = "#B80000"),symbol= 'none') %>% 
+            e_line(theta_ws, lineStyle = list(type = "solid", color = "#8d89f5"),symbol= 'none',name = 'wage') %>%
+            e_line(theta_vs, lineStyle = list(type = "solid", color = "#B80000"),symbol= 'none', name = 'Vacancy') %>% 
             e_hide_grid_lines() %>% 
             e_x_axis(min = 0.82) %>% 
             e_mark_p(
@@ -181,30 +199,30 @@ server <- function(input, output) {
         }
         
         if(input$param_increase == 'prod'){
-        echarts4rProxy("ws_vs_plot", data = dat %>% filter(between(w,w_range[1], w_range[2])), x = w) %>% 
-            e_line(theta_ws_y_plus) %>%
-            e_line(theta_vs_y_plus) %>% 
-            e_remove_serie("theta_vs_a_minus") %>%
-            e_remove_serie("theta_ws_beta_plus") %>%
-            e_remove_serie("theta_bc_a_plus") %>%
-            e_remove_serie("theta_ws_kappa_plus") %>%
-            e_remove_serie("theta_ws_beta_minus") %>%
-            e_remove_serie("theta_ws_y_minus") %>%
-            e_remove_serie("theta_vs_kappa_plus") %>%
-            e_remove_serie("theta_ws_b_plus") %>%
-            e_remove_serie("theta_vs_y_minus") %>%
-            e_remove_serie("theta_ws_kappa_minus") %>%
-            e_remove_serie("theta_ws_b_minus") %>%
-            e_remove_serie("theta_vs_a_plus") %>%
-            e_remove_serie("theta_vs_kappa_minus") %>%
-            e_remove_serie("theta_vs_lambda_plus") %>%
-            e_remove_serie("theta_vs_lambda_minus") %>%
-            e_execute()
+            echarts4rProxy("ws_vs_plot", data = dat %>% filter(between(w,w_range[1], w_range[2])), x = w) %>% 
+                e_line(theta_ws_y_plus, name = 'Wage') %>%
+                e_line(theta_vs_y_plus, name = 'Vacancy') %>% 
+                e_remove_serie("theta_vs_a_minus") %>%
+                e_remove_serie("theta_ws_beta_plus") %>%
+                e_remove_serie("theta_bc_a_plus") %>%
+                e_remove_serie("theta_ws_kappa_plus") %>%
+                e_remove_serie("theta_ws_beta_minus") %>%
+                e_remove_serie("theta_ws_y_minus") %>%
+                e_remove_serie("theta_vs_kappa_plus") %>%
+                e_remove_serie("theta_ws_b_plus") %>%
+                e_remove_serie("theta_vs_y_minus") %>%
+                e_remove_serie("theta_ws_kappa_minus") %>%
+                e_remove_serie("theta_ws_b_minus") %>%
+                e_remove_serie("theta_vs_a_plus") %>%
+                e_remove_serie("theta_vs_kappa_minus") %>%
+                e_remove_serie("theta_vs_lambda_plus") %>%
+                e_remove_serie("theta_vs_lambda_minus") %>%
+                e_execute()
         }
         
         if(input$param_increase == 'a'){
             echarts4rProxy("ws_vs_plot", data = dat %>% filter(between(w,w_range[1], w_range[2])), x = w) %>% 
-                e_line(theta_vs_a_plus) %>%  
+                e_line(theta_vs_a_plus, name = 'Vacancy') %>%  
                 e_remove_serie("theta_ws_y_plus") %>%
                 e_remove_serie("theta_vs_y_plus") %>%
                 e_remove_serie("theta_vs_a_minus") %>%
@@ -226,8 +244,8 @@ server <- function(input, output) {
         
         if(input$param_increase == 'kappa'){
             echarts4rProxy("ws_vs_plot", data = dat %>% filter(between(w,w_range[1], w_range[2])), x = w) %>% 
-                e_line(theta_ws_kappa_plus) %>% 
-                e_line(theta_vs_kappa_plus) %>% 
+                e_line(theta_ws_kappa_plus, name = 'Wage') %>% 
+                e_line(theta_vs_kappa_plus, name = 'Vacancy') %>% 
                 e_remove_serie("theta_ws_y_plus") %>%
                 e_remove_serie("theta_vs_y_plus") %>%
                 e_remove_serie("theta_ws_y_minus") %>%
@@ -247,7 +265,7 @@ server <- function(input, output) {
         
         if(input$param_increase == 'beta'){
             echarts4rProxy("ws_vs_plot", data = dat %>% filter(between(w,w_range[1], w_range[2])), x = w) %>% 
-                e_line(theta_ws_beta_plus) %>%  
+                e_line(theta_ws_beta_plus, name = 'Wage') %>%  
                 e_remove_serie("theta_ws_y_plus") %>%
                 e_remove_serie("theta_vs_y_plus") %>%
                 e_remove_serie("theta_ws_y_minus") %>%
@@ -268,7 +286,7 @@ server <- function(input, output) {
         
         if(input$param_increase == 'b'){
             echarts4rProxy("ws_vs_plot", data = dat %>% filter(between(w,w_range[1], w_range[2])), x = w) %>% 
-                e_line(theta_ws_b_plus) %>%
+                e_line(theta_ws_b_plus, name = 'Wage') %>%
                 e_remove_serie("theta_ws_y_plus") %>%
                 e_remove_serie("theta_vs_y_plus") %>%
                 e_remove_serie("theta_ws_y_minus") %>%
@@ -289,7 +307,7 @@ server <- function(input, output) {
         
         if(input$param_increase == 'lambda'){
             echarts4rProxy("ws_vs_plot", data = dat %>% filter(between(w,w_range[1], w_range[2])), x = w) %>% 
-                e_line(theta_vs_lambda_plus) %>% 
+                e_line(theta_vs_lambda_plus, name = 'Vacancy') %>% 
                 e_remove_serie("theta_ws_y_plus") %>%
                 e_remove_serie("theta_vs_y_plus") %>%
                 e_remove_serie("theta_ws_y_minus") %>%
